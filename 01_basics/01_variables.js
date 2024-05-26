e50ea52e39d2cfa52a1e9d2cb037147265a6eda2
Prashant Lad <48719396+theprashantlad@users.noreply.github.com>
2024-05-26 15:37:02 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 789456
+let accountEmail = "pprashantlad@gmail.com"
+var accountPassword = "123456"
+accountCity = "Mumbai"
+
+// accountId = 3 // not allowed
+accountEmail = "reena@gmail.com"
+accountPassword = "212121"
+accountCity = "Jaipur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
